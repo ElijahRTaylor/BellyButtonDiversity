c5e66007313ce226fb687c670fa6ab65c13f7a75
Elijah Taylor <elijahtaylor@MacBook-Pro-2.lan>
2022-01-11 00:42:05 -0500

    Added gauge visualization

--- charts.js
+++ charts.js
@@ -65,14 +65,26 @@ function buildCharts(_sample) {
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var resultArray = bellyButtonSamples.filter(sample => sample.id == _sample);
 
+    // 1c. Create a variable that filters the metadata array for the object with the desired sample number.
+
+    var metaArray = data.metadata.filter(sample => sample.id == _sample);
+
     //  5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
+
+     // 2c. Create a variable that holds the first sample in the metadata array.
+
+     var metaresult = metaArray[0]; 
 
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otu_ids = result.otu_ids;
     var otu_labels = result.otu_labels;
     var sample_values = result.sample_values;
+
+    // 3. Create a variable that holds the washing frequency.
+    var washingfreq = parseFloat(metaresult.wfreq)
+    console.log(washingfreq)
 
 
     // 7. Create the yticks for the bar chart.
@@ -124,7 +136,24 @@ function buildCharts(_sample) {
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
+    // 4c. Create the trace for the gauge chart.
+    trace1={
+      domain: { x: [0, 1], y: [0, 10] },
+      value: washingfreq,
+      title: { text: "Washing Frequency"},
+      type: "indicator",
+      mode: "gauge+number"
+    };
+
+    var gaugeData = [trace1];
+
+     // 5c. Create the layout for the gauge chart.
+    var gaugeLayout = {width: 600, height: 450, margin: { t: 0, b: 0 }}
+     
+    // 6c. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout)
   });
+
 
 
 
